10bb0962cce1b51c67fc9c61222d01ec1a89676b
YudinYury <yudin.yury@gmail.com>
2017-06-27 11:43:10 +0300

    update les21hw1

--- les21_hw1.py
+++ les21_hw1.py
@@ -85,40 +85,42 @@
 
 import copy
 
-#############################################################################################################
+
+
 def get_cook_book_from_file(file_name):
-    cook_bk={}
-    one_ing_list={'ingridient_name': '', 'quantity': 0, 'measure': ''}
-    one_ing_list_to_add={}
+    cook_bk = {}
+    one_ing_dict = {'ingridient_name': '', 'quantity': 0, 'measure': ''}
+    one_ing_list_to_add = {}
     with open('recipes.txt') as f:
         for l in f:
-            culinary_name=l.strip()
+            culinary_name = l.strip()
             if culinary_name in cook_bk:
                 # print('Повтор рецепта !')
                 continue
             else:
-                cook_bk[culinary_name]=[]
+                cook_bk[culinary_name] = []
                 # print('добавляем блюдо =',cook_bk)
-            quant=int(f.readline())
+            quant = int(f.readline())
             for i in range(quant):
-                ing_list=f.readline().strip().split('|')
+                ing_list = f.readline().strip().split('|')
                 ing_list = list(map(str.strip, ing_list))
                 # print('ing_list =',ing_list)
-                one_ing_list['ingridient_name']=ing_list[0]
-                one_ing_list['quantity']=int(ing_list[1])
-                one_ing_list['measure']=ing_list[2]
+                one_ing_dict['ingridient_name'] = ing_list[0]
+                one_ing_dict['quantity'] = int(ing_list[1])
+                one_ing_dict['measure'] = ing_list[2]
 
                 # print(type(one_ing_list), 'one_ing_list =', one_ing_list)
                 # print(type(cook_bk[culinary_name]), 'cook_bk[',culinary_name,'] =', cook_bk[culinary_name])
                 # print('----        Uuuuuuuuse .append()      ----')
-                one_ing_list_to_add=copy.deepcopy(one_ing_list)
+                one_ing_list_to_add=copy.deepcopy(one_ing_dict)
                 cook_bk[culinary_name].append(one_ing_list_to_add)
                 # print(type(cook_bk[culinary_name]), 'cook_bk[',culinary_name,'] =', cook_bk[culinary_name])
                 # print('cook_bk = ', cook_bk)
 
     return cook_bk
 
-#############################################################################################################
+
+
 def get_shop_list_by_dishes(ck_book, dishes, person_count):
   shop_list = {}
   for dish in dishes:
@@ -133,15 +135,20 @@ def get_shop_list_by_dishes(ck_book, dishes, person_count):
           k=int(new_shop_list_item['quantity'])
           shop_list[new_shop_list_item['ingridient_name']]['quantity'] +=new_shop_list_item['quantity']
   return shop_list
-#############################################################################################################
+
+
+
 def print_shop_list(shop_list):
     for shopp_values in shop_list.values():
         print('{}: {} {} '.format(shopp_values['ingridient_name'], shopp_values['quantity'],shopp_values['measure']))
-#############################################################################################################
-def get_dishes_for_person(lst):
 
+
+
+def get_dishes_for_person(lst):
     return dish
-#############################################################################################################
+
+
+
 def get_dish_list(ck_book):
     dish_list=[]
     dish_lst=[]
@@ -159,19 +166,12 @@ def get_dish_list(ck_book):
         dish_list.append(dish_lst[k])
 
     return dish_list
-#############################################################################################################
+
+
+
 
 def main():
     shop_lst={}
-    # f_name='recipes.txt'
-    # сформируем файл рецептов если его нет
-    # with open('recipes.txt', 'w') as f:
-    #     for key, val in cook_book.items():
-    #         f.write(str('{} \n{}\n'.format(key, len(cook_book[key]))))
-    #
-    #         for ingrad_key in cook_book[key]:
-    #             f.write(str('{} | {} | {}\n'.format(ingrad_key['ingridient_name'], ingrad_key['quantity'], ingrad_key['measure'])))
-    #     f.write(str('омлет\n3\nяйца | 2 | шт.\nмолоко | 50 | мл.\nпомидоры | 100 | гр.'))
 
     cook_book=get_cook_book_from_file('recipes.txt')
     # print(cook_book)
