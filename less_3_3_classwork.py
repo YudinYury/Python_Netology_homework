8acce3f8c70e6b0391dfacaf7a7a85448e4235c5
YudinYury <yudin.yury@gmail.com>
2017-07-12 14:34:07 +0300

    Lesson 3.3 classwork

---
+++ less_3_3_classwork.py
@@ -0,0 +1,48 @@
+"""lesson_3_3_classwork «Requests Lib and HTTP-requests»
+
+"""
+
+import chardet
+import collections
+import json
+import requests
+
+
+def translate_it(text):
+    """
+    YANDEX translation plugin
+    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
+    https://translate.yandex.net/api/v1.5/tr.json/translate ?
+    key=<API-ключ>
+     & text=<переводимый текст>
+     & lang=<направление перевода>
+     & [format=<формат текста>]
+     & [options=<опции перевода>]
+     & [callback=<имя callback-функции>]
+    :param text: <str> text for translation.
+    :return: <str> translated text.
+    """
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+    source_path = '3.2-requests'
+    params = {
+        'key': key,
+        'lang': 'ru-en',
+        'text': text,
+    }
+
+    response_raw = requests.get(url, params=params)
+    print('Body =', response_raw.content)
+    response = response_raw.json()
+
+    # response = requests.get(url, params=params).json()
+    return ' '.join(response.get('text', []))
+
+
+def main():
+    a = translate_it('Привет')
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
