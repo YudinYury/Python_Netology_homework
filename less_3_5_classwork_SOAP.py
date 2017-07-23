22e3954d3de74236285b2bcf1db6941d9d7cf545
YudinYury <yudin.yury@gmail.com>
2017-07-23 13:10:03 +0300

    lesson_3_5_classwork «API, xml/soap»

--- less_3_5_classwork_SOAP.py
+++ less_3_5_classwork_SOAP.py
@@ -1,4 +1,4 @@
-"""lesson_3_4_homework «API VK, Oauth protocol»
+"""lesson_3_5_classwork «API, xml/soap»
 
 """
 
@@ -9,6 +9,7 @@ import pprint
 import requests
 from urllib.parse import urlencode
 import osa
+from xml.etree.ElementTree import fromstring, ElementTree
 
 
 def mgramm_to_kgramm(mgramm):
@@ -23,14 +24,34 @@ def convert_weight(value, from_unit, to_unit):
         fromMetricWeightUnit=from_unit,
         toMetricWeightUnit=to_unit
     )
-
     return result
 
 
 def main():
-    print(
-        round(mgramm_to_kgramm(12_345_678), 2)
+    # print(round(mgramm_to_kgramm(1_234_567), 2))
+
+    response = requests.post('http://www.webservicex.net/convertMetricWeight.asmx?WSDL',
+                             headers={'Content-Type': 'text/xml; charset=utf-8'},
+                             data='''<?xml version="1.0" encoding="utf-8"?>
+                                    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                                      <soap:Body>
+                                        <ChangeMetricWeightUnit xmlns="http://www.webserviceX.NET/">
+                                          <MetricWeightValue>1234567</MetricWeightValue>
+                                          <fromMetricWeightUnit>milligram</fromMetricWeightUnit>
+                                          <toMetricWeightUnit>kilogram</toMetricWeightUnit>
+                                        </ChangeMetricWeightUnit>
+                                      </soap:Body>
+                                    </soap:Envelope>
+                             '''
     )
+    # tree = ElementTree(fromstring(response.text))
+    # tree = fromstring(response.text)
+    # el = list(list(list(tree)[0])[0])
+    # print(el.text)
+
+    # print(response.text)
+
+    print(convert_weight(1234567, 'milligram', 'kilogram'))
 
 
 if __name__ == '__main__':
