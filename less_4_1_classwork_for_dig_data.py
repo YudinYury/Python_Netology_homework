55fd2b721425916d1a887415a81c05a42ceed990
YudinYury <yudin.yury@gmail.com>
2017-08-06 22:25:29 +0300

    lesson_4_1_Classwork

--- less_4_1_classwork_for_dig_data.py
+++ less_4_1_classwork_for_dig_data.py
@@ -2,16 +2,39 @@
 
 """
 
-from functools import lru_cache
+import os
+
+import pandas as pd
 
 
-@lru_cache(maxsize=1)
-def calc(arg):
-    print('calc', arg)
-    return arg
+def count_of_len(row):
+    return len(row.Name)
 
 
-print('res', calc(1))
-print('res', calc(1))
-print('res', calc(2))
-print('res', calc(1))
+def main():
+    source_path = 'D:\Python_my\Python_Netology_homework\data_names'
+
+    source_dir_path = os.path.normpath(os.path.abspath(source_path))
+    source_file = os.path.normpath(os.path.join(source_dir_path, 'yob1900.txt'))
+    names = pd.read_csv(source_file, names=['Name', 'Gender', 'Count'])
+    # print(names.head(10))
+    # print(names.query('Count > 5000 & Gender == "M"'))
+    # print(names[(names.Count > 5000) | (names.Gender == 'M')].head(10))
+    # print(names[(names.Count > 5000) | (names.Name.str.startswith('M'))].head(10))
+    print(names[(names.Count > 3000) & (names.Name.str.startswith('M'))].head(10))
+    # dest_file = os.path.normpath(os.path.join(source_dir_path, 'yob1900.csv'))
+    # names[(names.Count > 3000) & (names.Name.str.startswith('M'))].to_csv(dest_file, index=False) # добавить параметр header=False, чтобы не записывать в файл шапку таблицы
+
+    # print(names[(names.Count > 3000) & (names.Name.str.startswith('M'))].sort_values(by=Name, ascending=False))
+    # print(names.sort_values(by=Name, ascending=False))
+    print()
+    # print(names.query('Gender == "M"').Count.sum())
+    # names['Len'] = names.apply(count_of_len, axis=1).head(10) #  axis=1 - это обход по строкам
+    names['Len'] = names.apply(lambda row: len(row.Name), axis=1).head(10)  # axis=1 - это обход по строкам
+    print(names.head(10))
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ less_4_1_classwork_merge_data.py
@@ -0,0 +1,63 @@
+"""lesson_4_1_Classwork "Data processing tools"
+
+"""
+
+import os
+
+import pandas as pd
+
+
+def count_of_len(row):
+    return len(row.Name)
+
+
+def agg_count(row):
+    # row.Count += row.Count_1900 + row.Count_1950 + row.Count
+    return row.Count_1900 + row.Count_1950 + row.Count
+
+
+def main():
+    source_path = 'D:\Python_my\Python_Netology_homework\data_names'
+    source_dir_path = os.path.normpath(os.path.abspath(source_path))
+    # source_file_1 = os.path.normpath(os.path.join(source_dir_path, 'yob1880.txt'))
+    # source_file_2 = os.path.normpath(os.path.join(source_dir_path, 'yob1890.txt'))
+    # columns = ['Name','Gender', 'Count']
+    # names_1880 = pd.read_csv(source_file_1, names=columns)
+    # names_1890 = pd.read_csv(source_file_2, names=columns)
+    # names_1880_1890 = pd.merge(names_1880, names_1890, on=['Name','Gender'], suffixes=('_1880', '_1890')).head(10)
+    # print(names_1880_1890)
+
+    columns = ['Name', 'Gender', 'Count']
+    source_file_1 = os.path.normpath(os.path.join(source_dir_path, 'yob1900.txt'))
+    source_file_2 = os.path.normpath(os.path.join(source_dir_path, 'yob1950.txt'))
+    names_1900 = pd.read_csv(source_file_1, names=columns)
+    names_1950 = pd.read_csv(source_file_2, names=columns)
+    names_1900_1950 = pd.merge(names_1900, names_1950, on=['Name', 'Gender'], suffixes=('_1900', '_1950'))
+    # print(names_1900_1950.head(10))
+    # print()
+    source_file_3 = os.path.normpath(os.path.join(source_dir_path, 'yob2000.txt'))
+    names_2000 = pd.read_csv(source_file_3, names=columns)
+    names_1900_1950_2000 = pd.merge(names_1900_1950, names_2000, on=['Name', 'Gender'])
+    # print(names_1900_1950_2000.head(10))
+    # print()
+    names_1900_1950_2000['Count_sum'] = names_1900_1950_2000.apply(agg_count, axis=1)
+    # print(names_1900_1950_2000.head(10))
+    print()
+
+    names_all = pd.concat([names_1900, names_1950], names=['Year', 'Pos'])
+    print(names_all.head(10))
+    print('----------------  Sort by Count  ----------------------')
+    print(names_1900.sort_values(by='Count', ascending=False).head(10))
+
+    names_all = pd.concat({1900: names_1900, 1950: names_1950, 2000: names_2000}, names=['Year', 'Pos'])
+    print('----------------  Sort by Name  ----------------------')
+    print(names_1900.sort_values(by='Name', ascending=True).head(10))
+
+    print(names_all.groupby('Name').sum().sort_values(by='Count', ascending=False).head(10))
+    print(names_1900.groupby('Gender').sum())
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
