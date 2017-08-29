8353d55c980aea9c37dedf9787585978763715ab
YudinYury <yudin.yury@gmail.com>
2017-08-29 10:50:48 +0300

    graduate work, day 2

--- graduate_work.py
+++ graduate_work.py
@@ -39,6 +39,14 @@
 Восстанавливается если случился ReadTimeout
 Показывать в том числе группы, в которых есть общие друзья, но не более, чем N человек, где N задаётся в коде
 
+Псевдокод:
+Получаем список групп tim_leary -> tim_leary_groups_list
+Получаем список друзей tim_leary -> friends
+По каждому другу получаем список его групп и проверяем наличие group_id в списке tim_leary_groups_list:
+    при совпадении удаляем group_id из tim_leary_groups_list
+По оставшимся в tim_leary_groups_list номерам group_id ->  записываем в файл groups.json
+   ->
+
 """
 
 from time import sleep
@@ -54,12 +62,25 @@ class VkGroup():
     members_count = 0
 
 
+def about_vk_group(vk_api, vk_group_id):
+    vk_group = {
+        "name": '',
+        "gid": None,
+        "members_count": 0
+    }
+    tim_leary_groups_list = vk_api.groups.getMembers(group_id=vk_group_id, count=3)
+
+    return vk_group
+
+
 def main():
     vk_group = {
         "name": '',
         "gid": None,
         "members_count": 0
     }
+    vk_group_allowed = []
+    vk_group_forbidden = []
     vk_group_list = []
     tim_leary_id = 5030613
     tim_leary_first_name = ''
@@ -75,20 +96,30 @@ def main():
     # friends_lists = vk_api.friends.getLists(user_id=tim_leary_id, fields='nickname, contacts, status', count=3)
     # print(friends_lists)
 
-    tim_leary_groups_list = vk_api.groups.get(user_id=tim_leary_id, count=3)
-    print('{} {} состоит в {} группах:'.format(tim_leary_first_name, tim_leary_last_name, len(tim_leary_groups_list)))
+    # tim_leary_groups_list = vk_api.groups.get(user_id=tim_leary_id)
+    tim_leary_groups_list = vk_api.groups.get(user_id=tim_leary_id, count=9)
+    tim_leary_groups_numbers = tim_leary_groups_list.pop(0)
+    print('{} {} состоит в {} группах:'.format(tim_leary_first_name, tim_leary_last_name, tim_leary_groups_numbers))
+    # print(len(tim_leary_groups_list))
+    print(tim_leary_groups_list)
+    for i, item in enumerate(tim_leary_groups_list):
+        if i % 3 == 0:
+            sleep(1)
+        print('{}) {}'.format(i + 1, item))
+        vk_group_allowed.append(about_vk_group(vk_api, item))
+
 
     # friends = vk_api.friends.get(user_id=tim_leary_id, fields='nickname, contacts')
-    friends = vk_api.friends.get(user_id=tim_leary_id, fields='nickname, contacts', count=5)
-    print('Friends list is {} persons:'.format(len(friends)))
-    frnd_id = 0
-    for frnd in friends:
-        sleep(1)
-        print(frnd)
-        frnd_id = frnd['uid']
-        friend_groups_list = vk_api.groups.get(user_id=frnd_id, count=3)
-        print(friend_groups_list)
-        print('{} состоит в {} группах:'.format(frnd_id, len(friend_groups_list)))
+        # friends = vk_api.friends.get(user_id=tim_leary_id, fields='nickname, contacts', count=5)
+        # print('Friends list is {} persons:'.format(len(friends)))
+        # frnd_id = 0
+        # for frnd in friends:
+        #     sleep(1)
+        #     print(frnd)
+        #     frnd_id = frnd['uid']
+        #     friend_groups_list = vk_api.groups.get(user_id=frnd_id, count=3)
+        #     print(friend_groups_list)
+        #     print('{} состоит в {} группах:'.format(frnd_id, len(friend_groups_list)))
 
 
 
@@ -100,7 +131,6 @@ def main():
         # print('{} {}'.format(frnd['first_name'], friends['last_name']))
         # print('{} {} aka "{}" have id: {}'.format(frnd['first_name'], friends['last_name'], friends['nickname'], friends['user_id']))
 
-    sleep(1)
 
 
 if __name__ == '__main__':
